00c3f762c9d755f8d3b69c536e7865739a2bf230
Chris Lucas <chris@hhd.com.au>
2017-01-20 09:58:21 +1100

    feat: Added limit and ignore
    Added ignore column feature and limit rows

--- build/js/tablify-1.0.js
+++ build/js/tablify-1.0.js
@@ -1,4 +1,5 @@
-$.fn.tablify = function() {
+$.fn.tablify = function(limit) {
+    if (typeof(limit)==='undefined') limit = 0;
     const sheet = (function() {
         // Create the <style> tag
         var style = document.createElement("style");
@@ -15,6 +16,15 @@ $.fn.tablify = function() {
         return style.sheet;
     })();
 
+    var addRule = function(sheet, selector, rules){
+        if($.inArray("insertRule",sheet)) {
+            sheet.insertRule(selector + "{" + rules + "}", 0);
+        }
+        else if($.inArray("addRule",sheet)) {
+            sheet.addRule(selector, rules);
+        }
+    };
+
     return this.each(function() {
         $(this).addClass('tablify-wrap');
 
@@ -28,15 +38,19 @@ $.fn.tablify = function() {
         $('table > tr').wrapAll( "<tbody></tbody>" );
 
         $("thead th", table).each(function(index, element){
-            var selector = 'td:nth-of-type('+(index+1)+'):before ';
-            var rules = " content: '"+$(element).html()+"'; left: "+tdpadd+"; ";
-
-            if($.inArray("insertRule",sheet)) {
-                sheet.insertRule(selector + "{" + rules + "}", 0);
-            }
-            else if($.inArray("addRule",sheet)) {
-                sheet.addRule(selector, rules);
+            // Add header values to before element in css
+            if(!$(element).hasClass('tablify-ignore')){
+                var selector = 'td:nth-of-type('+(index+1)+'):before ';
+                var rules = " content: '"+$(element).html()+"'; left: "+tdpadd+"; ";
+                addRule(sheet, selector, rules);
+            } else {
+                $('td:nth-of-type('+(index+1)+')').addClass('tablify-ignore');
             }
         });
+        if(limit > 1){
+            $('tr').addClass('tablify-ignore');
+            for(i=1; i<=limit; i++)
+                $('tr:nth-of-type('+(i+1)+')').removeClass('tablify-ignore');
+        }
     });
 };

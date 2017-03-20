1222298553fa49ed346e746417df3d231fc0727f
Chris Lucas <chris@hhd.com.au>
2017-03-21 10:54:44 +1100

    refactor: Refactored Selector and Before elements
    Removed unique selector and replaced style injection with content data attribute for header rows

--- build/js/tablify-1.0.js
+++ build/js/tablify-1.0.js
@@ -8,63 +8,8 @@ $.fn.tablify = function(config) {
     if (typeof(config.limit)==='undefined') config.limit = 0;
     if (typeof(config.hidden)==='undefined') config.hidden = [];
 
-    const sheet = (function() {
-        // Create the <style> tag
-        var style = document.createElement("style");
-
-        // WebKit hack
-        style.appendChild(document.createTextNode(""));
-
-        style.type  = 'text/css';
-        style.rel   = 'stylesheet';
-        style.media = 'screen';
-
-        // Add the <style> element to the page
-        document.head.appendChild(style);
-        return style.sheet;
-    })();
-
-    var addRule = function(sheet, selector, rules){
-        if($.inArray("insertRule",sheet)) {
-            sheet.insertRule(selector + "{" + rules + "}", 0);
-        }
-        else if($.inArray("addRule",sheet)) {
-            sheet.addRule(selector, rules);
-        }
-    };
-
-    String.prototype.hashCode = function() {
-      var hash = 0, i, chr, len;
-      if (this.length === 0) return hash;
-      for (i = 0, len = this.length; i < len; i++) {
-        chr   = this.charCodeAt(i);
-        hash  = ((hash << 5) + hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-      }
-      return hash;
-    };
-
     return this.each(function(index) {
-        var selector = $(this)
-          .parents()
-          .map(function() { return this.tagName; })
-          .get()
-          .reverse()
-          .concat([this.nodeName])
-          .join("");
 
-        var id = $(this).attr("id");
-        if (id) {
-          selector += "#"+ id;
-        }
-
-        var classNames = $(this).attr("class");
-        if (classNames) {
-          selector += "." + $.trim(classNames).replace(/\s/gi, ".");
-        }
-        selector += index;
-        var uniqueId = "tablify-uq-"+selector.hashCode();
-        $(this).attr({id: "tablify-uq-"+selector.hashCode()})
         $(this).addClass('tablify-wrap');
 
         const table = this;
@@ -86,11 +31,12 @@ $.fn.tablify = function(config) {
             $('> tr', table).wrapAll( "<tbody></tbody>" );
 
             $("thead th", table).each(function(index, element){
+
                 // Add header values to before element in css
                 if((!$(element).hasClass('tablify-ignore') && config.hidden.length <= 0) || (config.hidden.length > 0 && config.hidden.indexOf(index+1) === -1)){
-                    var selector = "#"+uniqueId+'.tablify-wrap td:nth-of-type('+(index+1)+'):before ';
-                    var rules = " content: '"+$(element).html()+"'; ";
-                    addRule(sheet, selector, rules);
+                    $('tbody td:nth-of-type('+(index+1)+')', table).each(function(indexCell, tableCell){
+                        $(tableCell).attr('data-header',$(element).html())
+                    });
                 } else {
                     $('td:nth-of-type('+(index+1)+')', table).addClass('tablify-ignore');
                 }
